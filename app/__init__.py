c398cd6bd71847c3125d35c94ca3a9df6788c859
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-11-15 20:07:07 -0800

    Endpoint '/ask' done end-to-end with categorizer model.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ import time, calendar
 from flask_api import FlaskAPI
 from flask import request, jsonify
 
+from categorizer import getCategories
+
 
 def create_app():
     app = FlaskAPI(__name__, instance_relative_config=True)
@@ -12,10 +14,19 @@ def create_app():
         if request.method == "POST":
             question = str(request.data.get('question'))
             if question:
+
+                que_cats = getCategories(question)
+                print('',end='')
+                print('================================================================================')
+                print('PREDICTED CATEGORIES FOR THE QUESTION')
+                print('================================================================================')
+                print(que_cats)
+
                 response = jsonify({
                     'question': question,
                     'timestamp': calendar.timegm(time.gmtime()),
-                    'answer': "Here is your answer..."
+                    'answer': "Here is your answer...",
+                    'message' : 'Check server console for QUE categories'
                 })
                 response.status_code = 200
                 return response
